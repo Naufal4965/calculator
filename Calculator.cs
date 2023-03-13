39a773e02efadfb0069392099ed11a4940d39b35
Naufal4965 <127710312+Naufal4965@users.noreply.github.com>
2023-03-13 16:43:58 +0700

    Update Calculator.cs

--- Calculator.cs
+++ Calculator.cs
@@ -2,6 +2,17 @@ class calculator
 {
 	static void Main(string[] args)
 	{
+		int a = 10;
+		int b = 6;
 		
 	}
+	static Penambahan(int a, int b)
+	{
+		return a+b;	
+	}
+	static Pengurangan(int a, int b)
+	{
+		return a-b;
+	}
+	
 }
