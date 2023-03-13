bc0252e84b7b0906bf9e940c7e2897d7ed501058
Naufal4965 <127710312+Naufal4965@users.noreply.github.com>
2023-03-13 16:47:01 +0700

    Update Calculator.cs
    menambahkan fungsi pengurangan

--- Calculator.cs
+++ Calculator.cs
@@ -2,11 +2,15 @@ class calculator
 {
 	static void Main(string[] args)
 	{
-				
+		
 	}
 	static Penambahan(int a, int b)
 	{
 		return a+b;	
+	}
+	static pengurangan(int a,int b)
+	{
+		return a-b;
 	}
 	
 }
