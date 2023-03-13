d34c94259beb4a7baa777a27fb1aadf7968ca0a6
Naufal4965 <127710312+Naufal4965@users.noreply.github.com>
2023-03-13 16:45:17 +0700

    Update Calculator.cs
    menambahkan fungsi pertambahan

--- Calculator.cs
+++ Calculator.cs
@@ -2,17 +2,11 @@ class calculator
 {
 	static void Main(string[] args)
 	{
-		int a = 10;
-		int b = 6;
-		
+				
 	}
 	static Penambahan(int a, int b)
 	{
 		return a+b;	
-	}
-	static Pengurangan(int a, int b)
-	{
-		return a-b;
 	}
 	
 }
